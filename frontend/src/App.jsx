50b28cd366c9ca28f3d482d2e724b1a99554a1ba
Tilok <tilokchandra988@gmail.com>
2026-06-29 16:08:51 +0530

    landing removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import "./index.css";
 import Home from "./pages/home/Home";
-import Landing from "./pages/landing/Landing";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Route, Routes, Navigate } from "react-router-dom";
@@ -13,10 +12,9 @@ function App() {
   return (
     <div className="h-screen bg-yellow-100">
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/chat" element={authUser ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/login" element={authUser ? <Navigate to="/chat" /> : <Login />} />
-        <Route path="/signup" element={authUser ? <Navigate to="/chat" /> : <SignUp />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp />} />
       </Routes>
       <Toaster />
     </div>
